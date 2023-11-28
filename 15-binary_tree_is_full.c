0822e0be4098aaaf263c9d8d57e377a1d8178e3a
Hisham Balboul <hishamsabry-97@hotmail.com>
2023-11-28 09:00:35 -0800

    15 Taskk

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,13 +9,12 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree != NULL)
-	{
-		if ((tree->left != NULL && tree->right == NULL) ||
-		    (tree->left == NULL && tree->right != NULL) ||
-		    is_full_recursive(tree->left) == 0 ||
-		    is_full_recursive(tree->right) == 0)
-			return (0);
-	}
-	return (1);
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left != NULL && tree->right != NULL)
+		return(binary_tree_is_full(tree->left) &&
+		       binary_tree_is_full(tree->right));
+	return (0);
 }
